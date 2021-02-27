7f4c4dd832eee47260a0cd0744f2c2c530acd892
Angel ILIEV <a.v.iliev13@gmail.com>
2021-02-27 06:27:43 -0800

    Removed usages of ioutil

--- kigo.go
+++ kigo.go
@@ -2,94 +2,104 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
-	"os"
-	"strings"
-	"regexp"
 	"hash/fnv"
-	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
-
 
 func FormatPattern(pattern string) string {
 	// Escape all "." characters
-	pattern =	strings.Replace(pattern, ".", "\\.", -1)
-	
+	pattern = strings.Replace(pattern, ".", "\\.", -1)
+
 	// Convert match all operator "*" to valid regex ".*"
-	pattern =	strings.Replace(pattern, "*", ".*", -1)
-			
+	pattern = strings.Replace(pattern, "*", ".*", -1)
+
 	return pattern
 }
 
 // PatternsInPath Checks if any of the provided patterns is found in the path.
-func PatternsInPath(patterns []string, path string) bool {	
+func PatternsInPath(patterns []string, path string) bool {
 	for _, pattern := range patterns {
 		match, err := regexp.MatchString(pattern, path)
 		if err != nil {
-			panic(fmt.Errorf("%v", err))
+			log.Fatal(err)
 		}
-		
+
 		if match {
 			return true
 		}
-	} 
+	}
 	return false
 }
-
 
 func GetFileHash(path string) (uint64, error) {
 	var fileHash uint64 = 0
 
 	// this reads the whole file in memory
-	contents, err := ioutil.ReadFile(path)
+	contents, err := os.ReadFile(path)
 	if err != nil {
 		return fileHash, err
 	}
-	
+
 	h := fnv.New64a()
 	h.Write(contents)
 	fileHash = h.Sum64()
-	
+
 	return fileHash, nil
 }
 
-// var Files [string]int
+type FilesHash map[string]uint64
 
+func ComputeChanges(filesHash FilesHash, rootPath string, excludePatterns []string) ([]string, error) {
+	var changedFiles []string
 
-func main() {
-	fmt.Println("Hello world")
-
-	// parameters
-	root := "./"	
-	exclude_patterns := []string{"*.exe", "*.git/*"}
-
-	// Format exclude patterns into valid regex	
-	for i, pattern := range exclude_patterns {
-		exclude_patterns[i] = FormatPattern(pattern)
-	}
-	
-	fmt.Println(exclude_patterns)
-	
-	var files []string
-	
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() && !PatternsInPath(exclude_patterns, path) {
-			
-			hash, err := GetFileHash(path)
-			if err != nil {
-				panic(fmt.Errorf("%v", err))
-			}
-			fmt.Println(hash, path)
-			
-			files = append(files, path)
-			
-
+	err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() || PatternsInPath(excludePatterns, path) {
+			return nil
 		}
+
+		hash, err := GetFileHash(path)
+		if err != nil {
+			return fmt.Errorf("Error occured while trying to get hash for: %s: %v", path, err)
+		}
+
+		elem, ok := filesHash[path]
+		// if file is not in the hash map or the hashes don't match => register a file change
+		if !ok || elem != hash {
+			changedFiles = append(changedFiles, path)
+		}
+
+		// update hash for every file
+		filesHash[path] = hash
+
 		return nil
 	})
-	if err != nil {
-		panic(fmt.Errorf("%v", err))
+
+	return changedFiles, err
+}
+
+func main() {
+	// parameters
+	root := "./"
+	excludePatterns := []string{"*.exe", "*.git*"}
+
+	// Format exclude patterns into valid regex
+	for i, pattern := range excludePatterns {
+		excludePatterns[i] = FormatPattern(pattern)
 	}
-	
-	fmt.Println(files)
+
+	fmt.Println(excludePatterns)
+
+	FilesHash := make(FilesHash)
+
+	changedFiles, err := ComputeChanges(FilesHash, root, excludePatterns)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(changedFiles)
+	fmt.Println(FilesHash)
 }
